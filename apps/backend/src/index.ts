2ad0ac58cf140420382961bed6cdc13f684a097c
Nawin <nawinsharma60@gmail.com>
2024-09-01 16:51:34 +0530

    add prisma orm and jest for test and a simple blog like app

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,13 +1,50 @@
-const express = require("express");
-const app = express();
-const port = 5000;
+import express, { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
+dotenv.config();
 
-app.get("/", (req:any, res:any) => {
-  res.send({
-    greetings: "Hello there!"
-  });
+const prisma = new PrismaClient();
+
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to Prisma, Express, and PSQL Tutorial");
+});
+
+// Create a blog
+app.post("/post", async (req: Request, res: Response) => {
+  try {
+    const { title, content } = req.body;
+    if (!title || !content) {
+      return res
+        .status(400)
+        .json({ message: "Please input Title and Content" });
+    }
+
+    const blog = await prisma.post.create({
+      data: { title, content },
+    });
+
+    return res
+      .status(201)
+      .json({ message: "Blog created successfully", data: blog });
+  } catch (error) {
+    return res.status(500).json({ message: "Error creating blog" });
+  }
+});
+
+// Get all blogs
+app.get("/posts", async (req: Request, res: Response) => {
+  try {
+    const blogs = await prisma.post.findMany();
+    return res.status(201).json({ data: blogs.length, blogs });
+  } catch (error) {
+    return res.status(500).json({ message: "Error fetching blogs" });
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server listening on ${port}`);
 });
