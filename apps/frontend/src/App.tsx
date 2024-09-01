535fe64635546134c3029d2ee4352c0c1d7d249e
Nawin <nawinsharma60@gmail.com>
2024-09-01 16:08:44 +0530

    add shadcn inside frontend project

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
 
   return (
    <>
    <div className='text-4xl text-green-500'>Buid your own X</div>
+   <Button className='mt-5 bg-blue-500 p-4'>Click me</Button>
    </>
   )
 }
--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+ 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
