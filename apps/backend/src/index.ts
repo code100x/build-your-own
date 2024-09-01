44fa883c4834dd8ffcb41f163663725e175d9963
Nawin <nawinsharma60@gmail.com>
2024-09-01 17:07:16 +0530

    add passport js authentication in backend

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import passport from "passport";
+const LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -9,8 +12,62 @@ const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
+// Use session middleware
+app.use(
+  session({
+    secret: "your-secret-key",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+// Initialize Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to Prisma, Express, and PSQL Tutorial");
+  res.send("Welcome");
+});
+
+const users = [{ id: 1, username: "user", password: "password" }];
+
+passport.use(
+  new LocalStrategy((username: string, password: string, done: any) => {
+    const user = users.find((u) => u.username === username);
+    if (!user) {
+      return done(null, false, { message: "Incorrect username." });
+    }
+    if (user.password !== password) {
+      return done(null, false, { message: "Incorrect password." });
+    }
+    return done(null, user);
+  })
+);
+
+passport.serializeUser((user: any, done: any) => {
+  done(null, user.id);
+});
+passport.deserializeUser((id: any, done: any) => {
+  const user = users.find((u) => u.id === id);
+  done(null, user);
+});
+
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/profile",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
+app.get("/profile", (req: any, res: any) => {
+  if (req.isAuthenticated()) {
+    res.send("Welcome to your profile");
+  } else {
+    res.redirect("/login");
+  }
+});
+app.get("/login", (req, res) => {
+  res.send("Login page");
 });
 
 // Create a blog
@@ -22,6 +79,16 @@ app.post("/post", async (req: Request, res: Response) => {
         .status(400)
         .json({ message: "Please input Title and Content" });
     }
+
+    function ensureAuthenticated(req: any, res: any, next: any) {
+      if (req.isAuthenticated()) {
+        return next();
+      }
+      res.redirect("/login");
+    }
+    app.get("/protected", ensureAuthenticated, (req, res) => {
+      res.send("This is a protected route");
+    });
 
     const blog = await prisma.post.create({
       data: { title, content },
