1b14f55561a72b8b9dfc46fe7555148e116e9b4e
Nawin <nawinsharma60@gmail.com>
2024-09-01 16:16:21 +0530

    add zustand for the state management inside frontend project

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,14 +1,23 @@
-import './App.css'
-import { Button } from './components/ui/button'
+import { Button } from './components/ui/button';
+import { useBookStore } from './store/booksStore';
+const App = () => {
 
-function App() {
+  const amount = useBookStore(state => state.amount)
+  const updateAmount = useBookStore(state => state.updateAmount)
 
   return (
-   <>
-   <div className='text-4xl text-green-500'>Buid your own X</div>
-   <Button className='mt-5 bg-blue-500 p-4'>Click me</Button>
-   </>
+    <div>
+      <div className='text-4xl text-green-500'>Buid your own X</div>
+      <Button className='mt-5 bg-blue-500 p-4'>Click me</Button>
+      <div className='mt-10'>
+        <h1> Books: {amount} </h1>
+        <Button
+          className='mt-5 bg-slate-500 p-4'
+          onClick={() => updateAmount(10)}
+        > Update Amount </Button>
+
+      </div>
+    </div>
   )
 }
-
 export default App
