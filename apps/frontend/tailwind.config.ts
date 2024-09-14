6844addcb3dbdd859d5fb61a57dfd882d89614d1
jashandeep <jashandeep1659@gmail.com>
2024-09-14 09:13:12 +0530

    chore: removed redundant code

--- apps/frontend/tailwind.config.ts
+++ apps/frontend/tailwind.config.ts
@@ -2,14 +2,4 @@ import type { Config } from "tailwindcss";
 import tailwindConfig from "@repo/ui/tailwind.config";
 export default {
   ...tailwindConfig,
-  theme: {
-    ...tailwindConfig.theme,
-    extend: {
-      ...tailwindConfig.theme.extend,
-      backgroundImage: {
-        "hero-bg": "url('/bg.png')",
-        "footer-texture": "url('/img/footer-texture.png')",
-      },
-    },
-  },
 } satisfies Config;
